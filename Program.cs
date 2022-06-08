d2913a3f17278bcb5d251f07e0f611bc041c35f3
Tsvetoslav <106446104+TsvetoslavAlexandrov@users.noreply.github.com>
2022-06-08 22:21:52 +0300

    SecondAttempt

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System;
+class MainClasdsss
+{
+    static void Main2(string[] args)
+    {
+
+        var str = Console.ReadLine();
+        var Stack = new Stack<char>();
+
+        foreach (var ch in str)
+        {
+            Stack.Push(ch);
+        }
+        while (Stack.Count > 0)
+        {
+            Console.Write(Stack.Pop());
+        }
+       
+
+
+    }
+}
